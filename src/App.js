d645502a49620772c1c94eda115ab407533e95c2
carl stokes <carlstokes@carls-MacBook-Pro.local>
2020-04-09 16:31:29 -0700

    commit first

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <>
+        <h1 styles={"backgroundColor: green"}>Welcome Turtles!!!</h1>
+        <p>
+          <span>FIND OUT WHAT TURTLE YOU ??? </span>
+        </p>
+        <div>Take the Quiz !!!</div>
+      </>
+      -{" "}
+    </div>
+    // quiz goes above ^^^
+  );
+}
+
+export default App;
---
+++ src/components/landing/landing.js
@@ -0,0 +1,3 @@
+import React, { Component } from "react";
+
+class Landing extends Component {}
