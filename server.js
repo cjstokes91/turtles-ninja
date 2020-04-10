d2cb8503829cd426dc2ecbe0b1fb7f7bcb6901ce
carl stokes <carlstokes@carls-MacBook-Pro.local>
2020-04-09 18:42:21 -0700

    user model

--- server.js
+++ server.js
@@ -5,15 +5,21 @@ const logger = require("morgan");
 
 const app = express();
 
+require("dotenv").config();
+require("./config/database");
+
 app.use(logger("dev"));
 app.use(express.json());
 
 app.use(favicon(path.join(__dirname, "build", "favicon.ico")));
 app.use(express.static(path.join(__dirname, "build")));
 
+app.use("api/users", require(".routes/api/users"));
+
 app.get("/*", function (req, res) {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
+
 //Note: Since this route is a "catch all" that matches every get request, be sure to mount API or other routes before it!
 
 const port = process.env.PORT || 3001;
