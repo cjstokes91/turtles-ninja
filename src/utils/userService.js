8a9087e7df14a4564f11c6b274ee7cbeba1c910b
carl stokes <carlstokes@carls-MacBook-Pro.local>
2020-04-10 17:40:12 -0700

    login form

--- src/utils/userService.js
+++ src/utils/userService.js
@@ -1,32 +1,20 @@
-import tokenService from "./tokenService";
-
-const BASE_URL = "/api/users/";
-
+import tokenService from './tokenService';
+const BASE_URL = '/api/users/';
 function signup(user) {
-  return (
-    fetch(BASE_URL + "signup", {
-      method: "POST",
-      headers: new Headers({ "Content-Type": "application/json" }),
-      body: JSON.stringify(user),
+  return fetch(BASE_URL + 'signup', {
+    method: 'POST',
+    headers: new Headers({ 'Content-Type': 'application/json' }),
+    body: JSON.stringify(user)
+  })
+    .then(res => {
+      if (res.ok) return res.json();
+      throw new Error('Email already taken!');
     })
-      .then((res) => {
-        if (res.ok) return res.json();
-        throw new Error("Email already taken!");
-      })
-      // Parameter destructuring!
-      .then(({ token }) => {
-        tokenService.setToken(token);
-      })
-  );
+    // Parameter destructuring!
+    .then(({ token }) => {
+      tokenService.setToken(token);
+    });
 }
-
-function getUser() {
-  return tokenService.getUserFromToken();
-}
-// the above could have been written as
-//.then((token) => token.token);
-
 export default {
-  signup,
-  getUser,
+  signup
 };
