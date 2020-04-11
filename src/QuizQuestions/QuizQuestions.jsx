0ba32065c51fba9fa6514258ae5a5b2043ed86f6
carl stokes <carlstokes@carls-MacBook-Pro.local>
2020-04-11 14:28:50 -0700

    added questions

---
+++ src/QuizQuestions/QuizQuestions.jsx
@@ -0,0 +1,125 @@
+let quizQuestions = [
+    {
+        question: "Whats your favorite pizza topping?",
+        answers: [
+            {
+                type: "Leonardo",
+                content: "sweet red onions, black olives, green peppers, mushrooms; Raphael: pepperoni, sausage"
+
+            },
+            {
+                type: "Raphael",
+                content: "pepperoni, Italian sausage, bacon, and beef"
+
+            },
+            {
+                type: "Donatello",
+                content: "chicken, mushrooms, green peppers, Roma tomatoes"
+
+            },
+            {
+                type: "Michelangelo",
+                content: " pepperoni, smoked ham, pineapple, and jalepenos"
+
+            },
+            {
+                type: "Splinter",
+                content: "tofu, almond cheese, roasted potatoes and roasted brussel sprouts"
+
+            }
+        ]
+    },
+    {
+
+        question: "Whats your favorite Color ?",
+        answers: [
+            {
+                type: "Leonardo",
+                content: "Blue"
+            },
+            {
+                type: "Raphael",
+                content: "Red"
+
+            },
+            {
+                type: "Donatello",
+                content: "Purple"
+
+            },
+            {
+                type: "Michelangelo",
+                content: "Orange"
+
+            },
+            {
+                type: "Splinter",
+                content: "Black"
+
+            }
+        ]
+    },
+    {
+        question: "Whats your utensil?",
+        answers: [
+            {
+                type: "Leonardo",
+                content: "Knife"
+
+            },
+            {
+                type: "Raphael",
+                content: "Fork"
+
+            },
+            {
+                type: "Donatello",
+                content: "Chopsticks"
+
+            },
+            {
+                type: "Michelangelo",
+                content: "Going Hands"
+
+            },
+            {
+                type: "Splinter",
+                content: "I'll just use my tail"
+
+            }
+        ]
+    },
+    {
+        question: "How would you prefer to spend a weekend",
+        answers: [
+            {
+                type: "Leonardo",
+                content: "Training to be the best, being lame"
+
+            },
+            {
+                type: "Raphael",
+                content: "Breaking up robberies in central park"
+
+            },
+            {
+                type: "Donatello",
+                content: "Coding a new app, inventing gadgets"
+
+            },
+            {
+                type: "Michelangelo",
+                content: "Video Games , sliding into april o'niels dm's"
+
+            },
+            {
+                type: "Splinter",
+                content: "Quite meditation"
+
+            }
+        ]
+
+    }
+]
+
+export default quizQuestions; 
--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -5,10 +5,53 @@ import userService from '../../utils/userService';
 import { Switch, Route } from 'react-router-dom';
 import NavBar from '../../components/NavBar/NavBar';
 import LoginPage from '../LoginPage/LoginPage';
+import Questions from '../../components/Questions/Questions'
+import QuizQuestions from '../../QuizQuestions/QuizQuestions';
+
 class App extends React.Component {
-  state = {
-    user: userService.getUser()
+
+  constructor(props) {
+    super(props);
   }
+  state = {
+    user: userService.getUser(),
+    counter: 0,
+    questionId: 1,
+    question: '',
+    answerOptions: [],
+    answer: '',
+    answersCount: {},
+    result: ''
+
+  }
+  componentDidMount() {
+    const shuffledAnswerOptions = QuizQuestions.map((question) => this.shuffleArray(question.answers));
+
+    this.setState({
+      question: QuizQuestions[0].question,
+      answerOptions: shuffledAnswerOptions[0]
+    });
+  }
+
+  shuffleArray(array) {
+    let currentIndex = array.length, temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+  };
+
   handleLogout = () => {
     userService.logout();
     this.setState({ user: null });
@@ -23,6 +66,7 @@ class App extends React.Component {
       <div className="App">
         <header>NINJA TURTLES</header>
         <NavBar user={this.state.user} handleLogout={this.handleLogout} />
+        <Questions content="what is your favorite food?" />
         <Route path='/signup' render={(props) => (
           <SignupForm {...props} />
         )
